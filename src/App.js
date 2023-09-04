70fe4faa4dc1257299fb644f066fdbb69142d79b
Erik-Cupsa <86483911+Erik-Cupsa@users.noreply.github.com>
2023-09-03 23:32:45 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import Progress from './components/Progress'
+import ChatComponent from './components/ChatComponent';
 
 function App() {
   useEffect(() => {
@@ -16,6 +17,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route index element={<Progress />} path = "/progress"/>
+          <Route index element={<ChatComponent />} path = "/chat"/>
         </Route>
       </Routes>
     </>
